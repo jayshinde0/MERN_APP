7573faa5ea06c5ac396bfdaa05ec78c9903e37e7
Jay Nitin Shinde <jayshinde4554@gmail.com>
2025-07-25 20:46:17 +0530

    update

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,11 +1,74 @@
-import React from 'react'
+import React from "react";
+import assets, { messagesDummyData } from "../assets/assets";
 
-const ChatContainer = () => {
-  return (
-    <div>
-      <h1>ChatContainer</h1>
+const ChatContainer = ({ selectedUser, setselecteduser }) => {
+  return selectedUser ? (
+    <div className="h-full overflow-scroll relative backdrop-blur-lg ">
+      <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-500">
+        <img src={assets.profile_martin} alt="" className="w-8 rounded-full" />
+        <p className="flex-1 text-lg text-white flex items-center gap-2">
+          Martin Smith
+          <span className="w-2 h-2 rounded-full bg-green-500"></span>
+        </p>
+        <img
+          onClick={() => setselecteduser(null)}
+          src={assets.arrow_icon}
+          alt=""
+          className="md:hidden max-w-7"
+        />
+        <img src={assets.help_icon} alt="" className="max-md:hidden max-w-5" />
+      </div>
+
+      {/* char area*/}
+      <div className="flex flex-col  p-3 h-[calc(100%-120px)] overflow-y-scroll pb-6 ">
+        {messagesDummyData.map((msg, index) => (
+          <div
+            key={index}
+            className={`flex items-end  gap-2 justify-end ${
+              msg.senderId === "680f50aaf10f3cd28382ecf2" && "flex-row-reverse"
+            }`}
+          >
+            {msg.image ? (
+              <img
+                src={msg.image}
+                alt=""
+                className="max-w-[230px] border broder-gray-700 rounded-lg overflow-hidden mb-8"
+              />
+            ) : (
+              <p
+                className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${
+                  msg.senderId === "680f50aaf10f3cd28382ecf2"
+                    ? "rounded-br-none"
+                    : "rounded-bl-none"
+                }`}
+              >
+                {msg.text}
+              </p>
+            )}
+            <div className="text-center text-xs">
+              <img src={msg.senderId=== '680f50aaf10f3cd28382ecf2' ? assets.avatar_icon : assets.profile_martin} alt="" className="w-7 rounded-full" />
+              <p className="text-gray-500">{msg.createdAt}</p>
+
+            </div>
+            <img
+              src={
+                msg.sender === "me"
+                  ? assets.profile_martin
+                  : assets.profile_john
+              }
+              alt=""
+              className="w-8 rounded-full"
+            />
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  ) : (
+    <div className="flex flex-col items-center justify-center h-full gap-2 text-gray-500 bg-white/10 max-md:hidden">
+      <img src={assets.logo_icon} alt="" className="max-w-16" />
+      <p className="text-lg font-medium text-white"> Chat anytime any where</p>
+    </div>
+  );
+};
 
-export default ChatContainer
+export default ChatContainer;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,22 +1,34 @@
-import React, { useState } from 'react'
-import SideBar from '../components/SideBar'
-import ChatContainer from '../components/ChatContainer'
-import RightSidebar from '../components/RightSidebar'
-
-
+import React, { useState } from "react";
+import SideBar from "../components/SideBar";
+import ChatContainer from "../components/ChatContainer";
+import RightSidebar from "../components/RightSidebar";
 
 const Home = () => {
-  const[selectedUser, setselecteduser]=useState(false)
+  const [selectedUser, setselecteduser] = useState(false);
   return (
-    <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
-     <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-full grid grid-col-1 relative ${selectedUser ?
-      'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' :'md:grid-cols-2'}`}>
-      <SideBar/>
-      <ChatContainer/>
-      <RightSidebar/>
-     </div>
+    <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
+      <div
+        className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-full grid grid-col-1 relative ${
+          selectedUser
+            ? "md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]"
+            : "md:grid-cols-2"
+        }`}
+      >
+        <SideBar
+          selectedUser={selectedUser}
+          setselecteduser={setselecteduser}
+        />
+        <ChatContainer
+          selectedUser={selectedUser}
+          setselecteduser={setselecteduser}
+        />
+        <RightSidebar
+          selectedUser={selectedUser}
+          setselecteduser={setselecteduser}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
