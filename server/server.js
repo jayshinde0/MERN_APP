cc7c8b609d0c7c7a8f6765e8f0ddb6c98d9d3784
Jay Nitin Shinde <jayshinde4554@gmail.com>
2025-07-27 20:38:31 +0530

    backend started

--- server/server.js
+++ server/server.js
@@ -3,3 +3,14 @@ import "dotenv/config";
 import cors from 'cors';
 import http from 'http';
 import { Server } from 'socket.io';
+
+const app = express();
+const server= http.createServer(app);
+
+// Middleware
+app.use(express.json({limit: '4mb'}));
+app.use(cors());
+
+app.use("/api/status",(res,req )=> res.send("Server is Live"));
+
+const port = process.env.PORT || 5000;
